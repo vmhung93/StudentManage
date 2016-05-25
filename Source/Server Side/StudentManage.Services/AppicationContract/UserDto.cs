15e0379fc56386fdb838b54771b47c2e589da9e9
Hung Vu <vmhung93@gmail.com>
2016-05-25 13:54:32 +0700

    Apply check authorization Update enum

--- Source/Server Side/StudentManage.Services/AppicationContract/UserDto.cs
+++ Source/Server Side/StudentManage.Services/AppicationContract/UserDto.cs
@@ -9,6 +9,8 @@ namespace StudentManage.Services.AppicationContract
 
         public string UserName { get; set; }
 
+        public string BadgeId { get; set; }
+
         public Guid AccessToken { get; set; }
 
         [Required]
